ebbe9f29bbdc1c349afabaa3f9951112dae07edb
KubaBiz <1kubi1@wp.pl>
2023-01-03 13:24:14 +0100

    lekkie korekty

--- src/main/java/evol/gen/World.java
+++ src/main/java/evol/gen/World.java
@@ -9,10 +9,6 @@ import java.util.ArrayList;
 public class World {
     public static void main(String[] args) {
 
-//        GrassField map = new GrassField(7,7,30);
-//        System.out.println(map);
-//       IEngine engine = new SimulationEngine(map,35);
-//       engine.run();
         Application.launch(App.class);
     }
 
